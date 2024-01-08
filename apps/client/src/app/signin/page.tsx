b3c483d008ebc87bc4f547b201f173a2c6239ded
Aditya Kumar <adityakumar172001@gmail.com>
2024-01-08 08:46:36 +0530

    update UI: update signin page

--- apps/client/src/app/signin/page.tsx
+++ apps/client/src/app/signin/page.tsx
@@ -4,16 +4,15 @@ import { FormSignin } from "ui";
 export default function SigninPage(): ReactElement {
   return (
     <div className="grid grid-col-3 grid-flow-col h-screen">
-      <div className="xl:flex items-center justify-center hidden xl:visible xl:col-span-1 bg-black text-3xl font-bold text-neutral-100">
+      <div className="md:flex items-center justify-center hidden md:visible md:col-span-1 bg-black text-3xl font-bold text-neutral-100">
         AlgoHub
       </div>
-      <div className="p-4 sm:p-0 col-span-3 xl:col-span-2 flex items-center justify-center">
+      <div className="p-4 sm:p-0 col-span-3 md:col-span-2 flex items-center justify-center">
         <div className="w-96">
-          <div className=" text-2xl font-bold text-center pb-8">Log In</div>
+          <div className=" text-2xl font-bold text-center pb-8">
+            Get Started
+          </div>
           <div className="rounded-md shadow-lg p-12 bg-neutral-800">
-            <div className="text-xl font-medium text-neutral-100 text-center pb-4">
-              Welcome
-            </div>
             <div className="text-sm font-light pb-4 text-center">
               Signin to continue to AlgoHub
             </div>
